9ef2f57f9d9774f24442c18ba0e578f51f33ba0a
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2025-10-04 21:28:46 +0800

    moar routes

--- webserver/routes/home.py
+++ webserver/routes/home.py
@@ -7,4 +7,6 @@ def home():
     return '''
     You are now at 0kayf1n3's page. Say hi at <a href="https://twitter.com/@0kayf1n3">Twitter</a>. 
     Convenience pages: <a href="/stats">stats</a>
+    <br>
+    This server is running v1.0.0.
     '''
