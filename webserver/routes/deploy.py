d56a3acd431be13e8ef85596f0f34ff7183a7527
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2025-10-04 21:28:39 +0800

    setup proper github verification

--- webserver/routes/deploy.py
+++ webserver/routes/deploy.py
@@ -11,16 +11,36 @@ from subprocess import Popen
 
 app = Blueprint('deploy', __name__)
 
+import hashlib
+import hmac
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        return "header was missing",403
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    print(f"{expected_signature=}")
+    if not hmac.compare_digest(expected_signature, signature_header):
+        return "mismatch in hash",403
+    return True
+
 @app.route('/deploy', methods=['GET', 'POST'])
 def stats():
     # detect if get or post
     if request.method != "POST":
         return "Why are you here?"
-    print(request.headers.get("X-Github-Encoded-Secret"))
-    print(DEPLOY_SECRET)
-    if request.headers.get('X-Github-Encoded-Secret') != DEPLOY_SECRET:
-        print("It's not GitHub")
-        return "Deploy failed!", 401
+    print( request.headers.get("X-Hub-Signature-256"))
+    print(request.headers)
+    signature = verify_signature(request.data, DEPLOY_SECRET, request.headers.get("X-Hub-Signature-256"))
+    if signature != True: return signature
     print("It's GitHub!")
     Popen([r"C:\Users\rfont\HOMELAB\deploy.bat"])
     return "Deploy sequence initiated."
