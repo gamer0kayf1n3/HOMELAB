e87c8ea09328650bb1f6365ef22fb215aa70850c
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2025-10-27 10:29:45 +0800

    debug file check

--- homeserver.py
+++ homeserver.py
@@ -1,6 +1,14 @@
 from webserver import create_app
+import os
+
+if os.path.exists("DEBUG"):
+    debug = True
+    port = 81
+else:
+    debug = False
+    port = 80
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=80, host='0.0.0.0', debug=False)
+    app.run(port=port, host='0.0.0.0', debug=debug)
